ee6555ede091d2355cf9b0b00962c4253e8e8a9f
MoncefME <ka_moussaoui@esi.dz>
2024-06-22 18:25:54 +0100

    folder structure - module skeleton

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { CustomerModule } from "./customer/customer.module";
+import { EmployeeModule } from "./employee/employee.module";
+import { RestaurantModule } from "./restaurant/restaurant.module";
+import { MenuModule } from "./menu/menu.module";
+import { OrderModule } from "./order/order.module";
+import { OrderItemModule } from "./order-item/order-item.module";
+import { MenuItemModule } from "./menu-item/menu-item.module";
+import { DatabaseModule } from "./database/database.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    CustomerModule,
+    EmployeeModule,
+    RestaurantModule,
+    MenuModule,
+    OrderModule,
+    OrderItemModule,
+    MenuItemModule,
+    DatabaseModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}

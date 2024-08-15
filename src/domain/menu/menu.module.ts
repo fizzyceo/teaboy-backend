7d687a2c2f002effc8ffe44ccbe4ce5cc4966a1f
MoncefME <ka_moussaoui@esi.dz>
2024-08-15 18:20:44 +0100

    encrypting menu_url

--- src/domain/menu/menu.module.ts
+++ src/domain/menu/menu.module.ts
@@ -3,10 +3,11 @@ import { MenuService } from "./menu.service";
 import { MenuController } from "./menu.controller";
 import { DatabaseModule } from "src/database/database.module";
 import { SpaceModule } from "../space/space.module";
+import { EncryptionModule } from "src/encryption/encryption.module";
 
 @Module({
   controllers: [MenuController],
   providers: [MenuService],
-  imports: [DatabaseModule, SpaceModule],
+  imports: [DatabaseModule, SpaceModule, EncryptionModule],
 })
 export class MenuModule {}
